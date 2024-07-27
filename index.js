d2e49aeb9e4180130a8b61e849a6a1840074eee8
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-07-27 16:56:27 +0530

    fuck mongoose. locak auth time

--- index.js
+++ index.js
@@ -1,103 +1,76 @@
 require('dotenv').config();
 const express = require('express');
-const { Client, RemoteAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const { MongoStore } = require('wwebjs-mongo');
-const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
 const apiKey = process.env.API_KEY;
 const sessionName = process.env.SESSION_SECRET;
 
-console.log('Connecting to MongoDB...');
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
-console.log('API_KEY:', process.env.API_KEY);
-console.log('SESSION_SECRET:', process.env.SESSION_SECRET);
 
-mongoose.connect(process.env.MONGODB_URI).then(async () => {
-    const store = new MongoStore({ mongoose: mongoose });
-    const client = new Client({
-        authStrategy: new RemoteAuth({
-            store: store,
-            backupSyncIntervalMs: 300000,
-            clientId:  'client1',
-            dataPath: './data/',
-        }),
-        restartOnAuthFail: true,
-        puppeteer: {
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        }
-    });
-
-    if(await store.sessionExists({session: sessionName})){
-        await store.extract({session: sessionName});
+const client = new Client({
+    authStrategy: new LocalAuth({
+        clientId: sessionName,
+        dataPath: './data/',
+    }),
+    restartOnAuthFail: true,
+    puppeteer: {
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
     }
-
-    client.on('qr', qr => {
-        console.log('No session found Locally. Please scan QR code to login.');
-        qrcode.generate(qr, { small: true });
-    });
-
-    client.on('authenticated', async () => {
-        // await store.save({ session: sessionName });
-        console.log('Authenticated!');
-    });
-    
-
-    client.on('ready', async() => {
-        console.log('Client is ready!');
-
-        // console.log('saving session')
-        // await store.save({session: sessionName});
-    });
-
-    client.on('remote_session_saved', async () => {
-        console.log('Session saved to database');
-    })
-
-    client.initialize();
-
-    app.use(express.json());
-
-    // Middleware to check API key
-    app.use((req, res, next) => {
-        const requestApiKey = req.headers['x-api-key'];
-        if (requestApiKey && requestApiKey === apiKey) {
-            next();
-        } else {
-            res.status(403).send('Forbidden: Invalid API Key');
-        }
-    });
-
-    app.post('/send-message', async (req, res) => {
-        const { groupId, message } = req.body;
-
-        try {
-            const chat = await client.getChatById(groupId);
-            await chat.sendMessage(message);
-            res.status(200).send('Message sent successfully');
-        } catch (error) {
-            console.error('Error sending message:', error);
-            res.status(500).send('Failed to send message');
-        }
-    });
-
-    app.get('/get-chats', async (req, res) => {
-        try {
-            const chats = await client.getChats();
-
-            const groupChats = chats.filter(chat => chat.isGroup);
-    
-            res.status(200).send(groupChats);
-        } catch (error) {
-            console.error('Error getting chats:', error);
-            res.status(500).send('Failed to get chats');
-        }
-    });
-
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
-    });
 });
+
+client.on('qr', qr => {
+    console.log('No session found Locally. Please scan QR code to login.');
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('authenticated', async () => {
+    console.log('Authenticated');
+});
+
+client.initialize();
+
+app.use(express.json());
+
+// Middleware to check API key
+app.use((req, res, next) => {
+    const requestApiKey = req.headers['x-api-key'];
+    if (requestApiKey && requestApiKey === apiKey) {
+        next();
+    } else {
+        res.status(403).send('Forbidden: Invalid API Key');
+    }
+});
+
+app.post('/send-message', async (req, res) => {
+    const { groupId, message } = req.body;
+
+    try {
+        const chat = await client.getChatById(groupId);
+        await chat.sendMessage(message);
+        res.status(200).send('Message sent successfully');
+    } catch (error) {
+        console.error('Error sending message:', error);
+        res.status(500).send('Failed to send message');
+    }
+});
+
+app.get('/get-chats', async (req, res) => {
+    try {
+        const chats = await client.getChats();
+
+        const groupChats = chats.filter(chat => chat.isGroup);
+
+        res.status(200).send(groupChats);
+    } catch (error) {
+        console.error('Error getting chats:', error);
+        res.status(500).send('Failed to get chats');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
