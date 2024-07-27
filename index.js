007e60363534794f5588607a080d7e45d21840bc
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-07-27 19:10:47 +0530

    implemented client

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ const port = process.env.PORT || 3000;
 const apiKey = process.env.API_KEY;
 const sessionName = process.env.SESSION_SECRET;
 
+console.log('client is starting');
 
 const client = new Client({
     authStrategy: new LocalAuth({
         clientId: sessionName,
         dataPath: './data/',
     }),
-    restartOnAuthFail: true,
     puppeteer: {
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        headless: false,
+        executablePath: '/usr/bin/google-chrome',
+        args: ['--disable-gpu', '--no-sandbox'],
     }
 });
 
@@ -26,51 +27,59 @@ client.on('qr', qr => {
     qrcode.generate(qr, { small: true });
 });
 
-client.on('authenticated', async () => {
+client.on('authenticated', () => {
     console.log('Authenticated');
+    console.log('Saving session to local storage');
 });
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+
+    app.use(express.json());
+
+    // Middleware to check API key
+    app.use((req, res, next) => {
+        const requestApiKey = req.headers['x-api-key'];
+        if (requestApiKey && requestApiKey === apiKey) {
+            next();
+        } else {
+            res.status(403).send('Forbidden: Invalid API Key');
+        }
+    });
+    
+    app.post('/send-message', async (req, res) => {
+        const { groupId, message } = req.body;
+    
+        try {
+            const chat = await client.getChatById(groupId);
+            await chat.sendMessage(message);
+            res.status(200).send('Message sent successfully');
+        } catch (error) {
+            console.error('Error sending message:', error);
+            res.status(500).send('Failed to send message');
+        }
+    });
+    
+    app.get('/get-chats', async (req, res) => {
+        try {
+            const chats = await client.getChats();
+    
+            const groupChats = chats.filter(chat => chat.isGroup);
+    
+            res.status(200).send(groupChats);
+        } catch (error) {
+            console.error('Error getting chats:', error);
+            res.status(500).send('Failed to get chats');
+        }
+    });
+    
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+    
+});
+
 
 client.initialize();
 
-app.use(express.json());
-
-// Middleware to check API key
-app.use((req, res, next) => {
-    const requestApiKey = req.headers['x-api-key'];
-    if (requestApiKey && requestApiKey === apiKey) {
-        next();
-    } else {
-        res.status(403).send('Forbidden: Invalid API Key');
-    }
-});
-
-app.post('/send-message', async (req, res) => {
-    const { groupId, message } = req.body;
-
-    try {
-        const chat = await client.getChatById(groupId);
-        await chat.sendMessage(message);
-        res.status(200).send('Message sent successfully');
-    } catch (error) {
-        console.error('Error sending message:', error);
-        res.status(500).send('Failed to send message');
-    }
-});
-
-app.get('/get-chats', async (req, res) => {
-    try {
-        const chats = await client.getChats();
-
-        const groupChats = chats.filter(chat => chat.isGroup);
-
-        res.status(200).send(groupChats);
-    } catch (error) {
-        console.error('Error getting chats:', error);
-        res.status(500).send('Failed to get chats');
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
 
